c0c76e8855a7c2429225c5c672e961611c711bd5
Craig A. Markowitz <cmarkowitz881970@gmail.com>
2022-01-22 11:30:21 -0500

    Added Welcome Screen but did not do anything with it yet

---
+++ app_1.js
@@ -0,0 +1,7 @@
+import React from "react";
+import { View } from "react-native";
+import WelcomeScreen from "./src/screens/WelcomeScreen";
+
+export default function App(){
+    return <WelcomeScreen />;
+}
